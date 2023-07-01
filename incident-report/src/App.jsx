e2a01568665c78f46fd744bf78c0374e94bc5b7d
Thomas Vesser <Vesser00@me.com>
2023-07-01 17:07:40 -0500

    Added map markers

--- incident-report/src/App.jsx
+++ incident-report/src/App.jsx
@@ -1,6 +1,10 @@
 import './App.css'
 import { Loader } from '@googlemaps/js-api-loader';
 import reports from "./data/reports.json"
+import IncidentPin from "./img/alarm.png"
+import EnginePin from "./img/Engine.png"
+import ArrivedPin from "./img/Arrived.png"
+import FireStationPin from "./img/fire-station.png"
 
 const apiOptions = {
     apiKey: "AIzaSyBv_ADwQqSzPOGZXcpOefKs3CyPk5WL_uU"
@@ -11,7 +15,8 @@ const loader = new Loader(apiOptions)
 loader.load().then(() => {
     console.log("Maps JS API loaded")
     const map = displayMap()
-    console.log(reports)
+    const markers = addMarkers(map)
+    // console.log(reports)
 })
 
 fetch('./data')
@@ -20,10 +25,52 @@ function displayMap() {
     const mapOptions = {
         center: { lat: reports.address.latitude, lng: reports.address.longitude },
         zoom: 15,
-        mapId: 'incident-report-dark',
+        mapId: 'night',
     }
 
     const mapDiv = document.getElementById('map')
 
     return new google.maps.Map(mapDiv, mapOptions)
+}
+
+function addMarkers(map) {
+    const markers = []
+    const markerOptions = {
+        map: map,
+        position: { lat: reports.address.latitude, lng: reports.address.longitude },
+        icon: IncidentPin
+    }
+    const incidentMarker = new google.maps.Marker(markerOptions)
+    markers.push(incidentMarker)
+
+    for (const apparatus of reports.apparatus) {
+        console.log(apparatus.unit_status)
+        const dispatched = {
+            map: map,
+            position: { lat: apparatus.unit_status.dispatched.latitude, lng: apparatus.unit_status.dispatched.longitude},
+            icon: FireStationPin
+        }
+        const arrived = {
+            map: map,
+            position: { lat: apparatus.unit_status.arrived.latitude, lng: apparatus.unit_status.arrived.longitude},
+            icon: EnginePin
+        }
+        const enroute = {
+            map: map,
+            position: { lat: apparatus.unit_status.enroute.latitude, lng: apparatus.unit_status.enroute.longitude},
+            icon: EnginePin
+        }
+        const available = {
+            map: map,
+            position: { lat: apparatus.unit_status.available.latitude, lng: apparatus.unit_status.available.longitude},
+            icon: EnginePin
+        }
+
+        markers.push(new google.maps.Marker(dispatched))
+        markers.push(new google.maps.Marker(arrived))
+        markers.push(new google.maps.Marker(enroute))
+        markers.push(new google.maps.Marker(available))
+    }
+
+    return markers
 }
